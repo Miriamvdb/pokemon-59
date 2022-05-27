5a06249420b07597cab2d631e4c4548f228194e2
Miriamvdb <miriamvdb@live.nl>
2022-05-27 17:57:51 +0200

    Added detailpages

--- src/pages/PokemonDetailPage.js
+++ src/pages/PokemonDetailPage.js
@@ -1,7 +1,36 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function PokemonDetailPage() {
+function PokemonDetailPage() {
   const routeParams = useParams();
+  const [pokemon, setPokemon] = useState(null);
 
-  return <p>{routeParams.pokemonName}</p>;
+  useEffect(() => {
+    async function getPokemonDetails() {
+      const pokemonResponse = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${routeParams.pokemonName}`
+      );
+      setPokemon(pokemonResponse.data);
+    }
+    getPokemonDetails();
+  }, []);
+
+  return pokemon ? (
+    <div>
+      <h2>{pokemon.name}</h2>
+      {/* <img src={pokemon.sprites.front_default} /> */}
+      <p>
+        Types:{" "}
+        {pokemon.types.map((typeObj) => (
+          <span>{typeObj.type.name} </span>
+        ))}
+      </p>
+      <p>Weight: {pokemon.weight} hectograms</p>
+    </div>
+  ) : (
+    <p>Loading ...</p>
+  );
 }
+
+export default PokemonDetailPage;
